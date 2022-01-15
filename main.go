a45a7e3c04c2370eabe7725663497b79c2b26cc8
Santo <hectorgool@gmail.com>
2022-01-15 13:03:37 -0600

    test struct

--- main.go
+++ main.go
@@ -13,5 +13,29 @@ type Display struct {
 }
 
 func main() {
-	fmt.Println("hola mundo")
+
+	var accountNumber AccountNumbers
+
+	accountNumber.AccountNumber[0].DisplayRow1[0] = "a"
+	accountNumber.AccountNumber[0].DisplayRow1[1] = "b"
+	accountNumber.AccountNumber[0].DisplayRow1[2] = "c"
+
+	accountNumber.AccountNumber[1].DisplayRow1[0] = "a"
+	accountNumber.AccountNumber[1].DisplayRow1[1] = "b"
+	accountNumber.AccountNumber[1].DisplayRow1[2] = "c"
+
+	accountNumber.AccountNumber[2].DisplayRow1[0] = "a"
+	accountNumber.AccountNumber[2].DisplayRow1[1] = "b"
+	accountNumber.AccountNumber[2].DisplayRow1[2] = "c"
+
+	accountNumber.AccountNumber[3].DisplayRow1[0] = "a"
+	accountNumber.AccountNumber[3].DisplayRow1[1] = "b"
+	accountNumber.AccountNumber[3].DisplayRow1[2] = "c"
+
+	accountNumber.AccountNumber[8].DisplayRow1[0] = "a"
+	accountNumber.AccountNumber[8].DisplayRow1[1] = "b"
+	accountNumber.AccountNumber[8].DisplayRow1[2] = "c"
+
+	fmt.Printf("\nLa estructura es:\n %v\n", accountNumber)
+
 }
