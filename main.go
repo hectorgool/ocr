c0bdb322131128b271b8d3ce4ca3c99e49cd9adf
Santo <hectorgool@gmail.com>
2022-01-15 13:30:15 -0600

    test func ReadFileNuberAccounts

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 type AccountNumbers struct {
 	AccountNumber [9]Display
@@ -14,24 +19,52 @@ type Display struct {
 
 func main() {
 
-	var accountNumber AccountNumbers
+	/*
+		var accountNumber AccountNumbers
 
-	for row := 0; row < 3; row++ {
-		for display := 0; display < 9; display++ {
-			for segment := 0; segment < 3; segment++ {
-				if row == 0 {
-					accountNumber.AccountNumber[display].DisplayRow1[segment] = "a"
-				}
-				if row == 1 {
-					accountNumber.AccountNumber[display].DisplayRow2[segment] = "b"
-				}
-				if row == 2 {
-					accountNumber.AccountNumber[display].DisplayRow3[segment] = "c"
+		for row := 0; row < 3; row++ {
+			for display := 0; display < 9; display++ {
+				for segment := 0; segment < 3; segment++ {
+					if row == 0 {
+						accountNumber.AccountNumber[display].DisplayRow1[segment] = "a"
+					}
+					if row == 1 {
+						accountNumber.AccountNumber[display].DisplayRow2[segment] = "b"
+					}
+					if row == 2 {
+						accountNumber.AccountNumber[display].DisplayRow3[segment] = "c"
+					}
 				}
 			}
 		}
+
+		fmt.Printf("\nLa estructura es:\n %v\n", accountNumber)
+	*/
+
+	rows := ReadFileNuberAccounts()
+
+	for i, row := range rows {
+		fmt.Printf("%v %v\n", i, row)
 	}
 
-	fmt.Printf("\nLa estructura es:\n %v\n", accountNumber)
+}
 
+func ReadFileNuberAccounts() []string {
+
+	file, err := os.Open("test_numbers.txt")
+	if err != nil {
+		log.Fatalf("failed to open")
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanLines)
+	var text []string
+
+	for scanner.Scan() {
+		text = append(text, scanner.Text())
+	}
+
+	file.Close()
+	return text
 }
