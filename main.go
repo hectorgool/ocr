eacdb9e63b29fed44dd9aad1c38873e14c467bda
Santo <hectorgool@gmail.com>
2022-01-16 09:09:35 -0600

    move enpoint /

--- main.go
+++ main.go
@@ -8,12 +8,7 @@ import (
 
 func main() {
 	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"data": "ocr",
-		})
-	})
+	r.GET("/", controller.GetRoot)
 	r.GET("/number", controller.GetAccountNumber)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
