aa76e9cab5dcaef1d3edf12c0628ad3118d714e1
Santo <hectorgool@gmail.com>
2022-01-25 10:48:20 -0600

    add new endpoint: /numbers

--- main.go
+++ main.go
@@ -10,5 +10,6 @@ func main() {
 	r := gin.Default()
 	r.GET("/", controller.GetRoot)
 	r.GET("/number", controller.GetAccountNumber)
+	r.POST("/numbers", controller.PostNumbers)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
